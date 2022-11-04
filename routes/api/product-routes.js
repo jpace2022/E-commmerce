aa399d8d4a9c51263ee112469b2f47e9199b9a28
James Pace <pacej2022@gmail.com>
2022-11-04 09:23:26 -0700

    updated models

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
-
 // get all products
+// find all products
+// be sure to include its associated Category and Tag data
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  
 });
 
 // get one product
