89bfb35e6b793ed1089dae4a7530942d7d7afea6
James Pace <pacej2022@gmail.com>
2022-11-04 20:49:32 -0700

    update product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -36,15 +36,8 @@ router.get('/:id', async (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      category_id: 3
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+    cosnol.log(req.body)
+    
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
